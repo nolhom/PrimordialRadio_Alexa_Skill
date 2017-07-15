c902714b81694d2bd1238a9d92512816d5ce125a
nolhom <ndoliver@gmail.com>
2017-07-15 13:59:55 +0100

    Add files via upload

---
+++ audioAssets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Audio Source - Primordial Radio
+var audioData = [
+    {
+        'title' : 'Primordial Test',
+        'url' : 'http://212.83.172.22/radio/58204/stream/95431.mp3'
+    }
+];
+
+module.exports = audioData;
